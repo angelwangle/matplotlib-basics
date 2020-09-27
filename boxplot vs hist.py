ee6e5490799b4081adbf4b51f3ac11ca8170834f
angelwangle <50367178+angelwangle@users.noreply.github.com>
2020-09-27 19:47:31 +0800

    Add files via upload

---
+++ boxplot vs hist.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 15:28:44 2020
+
+@author: User
+"""
+
+from pylab import *
+
+dataset = [113, 115, 119, 121, 124,
+           124, 125, 126, 126, 126,
+           127, 127, 128, 129, 130,
+           130, 131, 132, 133, 136]
+subplot(121)
+boxplot(dataset, vert = False)
+
+subplot(122)
+hist(dataset)
+
+show()
+
